c1d8a7c87fdf2ba946d64c9e33053d4660fd3b5d
Raveau Matthis <raveau@insa-toulouse.fr>
2022-12-08 09:49:59 +0100

    Passage de id en int à des id en string pour les ips

--- ChatBox/src/DatabaseManager.java
+++ ChatBox/src/DatabaseManager.java
@@ -32,8 +32,8 @@ public class DatabaseManager {
 		   stmt = conn.createStatement();
 		   String sql = "CREATE TABLE IF NOT EXISTS message" +
                    "(\n	IDMESSAGE integer PRIMARY KEY,\n" +
-                   " 	IDSENDER integer NOT NULL,\n" + 
-                   " 	IDRECV integer NOT NULL,\n" + 
+                   " 	IDSENDER text NOT NULL,\n" + 
+                   " 	IDRECV text NOT NULL,\n" + 
                    " 	CONTENU  text,\n" + 
                    "	DATEMESSAGE Timestamp\n)"; 
 		   stmt.executeUpdate(sql);
@@ -51,7 +51,7 @@ public class DatabaseManager {
 			Statement stmt = null;
 			stmt = conn.createStatement();
 			String sql = "CREATE TABLE IF NOT EXISTS users" +
-					"(\n IDUSERS integer PRIMARY KEY,\n" +
+					"(\n IDUSERS text PRIMARY KEY,\n" +
 					" 	LOGIN text\n)";
 			stmt.executeUpdate(sql);
 		}
@@ -64,11 +64,11 @@ public class DatabaseManager {
 
   
    
-   public void insertuser( int idUser,String login) {
+   public void insertuser( String idUser,String login) {
 		String sql = "INSERT INTO users VALUES(?,?)";
 		//(IDUSERS,LOGIN)
 		try ( PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			pstmt.setInt(1, idUser);
+			pstmt.setString(1, idUser);
 			pstmt.setString(2, login);
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
@@ -76,13 +76,13 @@ public class DatabaseManager {
 		}
 	}
 
-   public void insertmessage(int idmessage, int idsender, int idreceiver, String contenu, Timestamp Datetimemessage) {
+   public void insertmessage(int idmessage, String idsender, String idreceiver, String contenu, Timestamp Datetimemessage) {
 	   String sql = "INSERT INTO message VALUES(?,?,?,?,?)";
 
        try ( PreparedStatement pstmt = conn.prepareStatement(sql)) {
            pstmt.setInt(1,idmessage );
-           pstmt.setInt(2,idsender );
-           pstmt.setInt(3,idreceiver );
+           pstmt.setString(2,idsender );
+           pstmt.setString(3,idreceiver );
            pstmt.setString(4, contenu);
            pstmt.setTimestamp(5,Datetimemessage);
            pstmt.executeUpdate();
@@ -91,14 +91,14 @@ public class DatabaseManager {
        }
    }
    
-   public void changerPseudo(int idUser, String newlogin){
+   public void changerPseudo(String idUser, String newlogin){
 		String sql = "UPDATE users SET LOGIN = ? WHERE IDUSERS = ?";
 		//, WHERE IDUSERS = ?
 		try ( PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
 			// set the corresponding param
 			pstmt.setString(1, newlogin);
-			pstmt.setInt(2, idUser);
+			pstmt.setString(2, idUser);
 			// upDatetime
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
@@ -106,14 +106,14 @@ public class DatabaseManager {
 		}
 	}
    
-   public boolean IdExists(int idUser) {
+   public boolean IdExists(String idUser) {
 	    String sql = "SELECT LOGIN "
 				+ "FROM users WHERE IDUSERS = ?";
 
 		try ( PreparedStatement pstmt  = conn.prepareStatement(sql)){
 
 			// set the value
-			pstmt.setInt(1,idUser);
+			pstmt.setString(1,idUser);
 			//
 			ResultSet rs  = pstmt.executeQuery();
 
@@ -128,15 +128,15 @@ public class DatabaseManager {
    
    public void getIdByLogin(String login){
 
-		int id = getIdbyLoginInt(login);
-		if (id <0 ) {
+		String id = getIdbyLoginString(login);
+		if (id.equals("")) {
 			System.out.println("Ce login n'existe pas dans l'annuaire");
 		}
 		else System.out.println(id);
 	}
 
    
-   public int getIdbyLoginInt(String login) {
+   public String getIdbyLoginString(String login) {
 	   String sql = "SELECT IDUSERS "
 				+ "FROM users WHERE LOGIN = ?";
 
@@ -149,12 +149,12 @@ public class DatabaseManager {
 
 			// loop through the result set
 			while (rs.next()) {
-				return rs.getInt("IDUSERS");
+				return rs.getString("IDUSERS");
 			}
 		} catch (SQLException e) {
-			return -1;
+			return "";
 	}
-		return -1;
+		return "";
    }
    
    
@@ -182,7 +182,7 @@ public class DatabaseManager {
 	   else	System.out.println(getAnnuaireList());
 	}
    
-   public int getMaxIdmessage(int myId, int theirID) {
+   public int getMaxIdmessage(String myId, String theirID) {
 	   String sql = "SELECT IDMESSAGE FROM message WHERE (IDSENDER = ? AND IDRECV = ?) OR (IDSENDER = ? AND IDRECV = ?)";
 	   /*pstmt.setInt(4, myId);;
        pstmt.setInt(3, theirID);*/
@@ -192,7 +192,8 @@ public class DatabaseManager {
    
    
    
-   public void selectHistorywithX(int myId, int theirID){
+   //PAS BON (que idmessage et que dans un sens sender receiver
+   public void selectHistorywithX(String myId, String theirID){
 	   
        String sql = "SELECT IDMESSAGE, CONTENU, DATEMESSAGE FROM message WHERE IDSENDER = ? AND IDRECV = ?";
        //idmessage, idsender, idreceiver, contenu, Datetimemessage
@@ -200,8 +201,8 @@ public class DatabaseManager {
        try ( PreparedStatement pstmt  = conn.prepareStatement(sql)){
     	   
               // set the value
-              pstmt.setInt(1, myId);;
-              pstmt.setInt(2, theirID);
+              pstmt.setString(1, myId);;
+              pstmt.setString(2, theirID);
           
               ResultSet rs  = pstmt.executeQuery();
               // loop through the result set
@@ -214,6 +215,51 @@ public class DatabaseManager {
 
    }
    
+   
+   public ArrayList<Message> ArrayHistorywithX(String myId, String theirID){
+		ArrayList<Message> result = new ArrayList<Message>();
+
+		String sql = "SELECT IDSENDER, IDRECV, CONTENU, DATEMESSAGE FROM message WHERE (IDSENDER = ? AND IDRECV = ) OR (IDSENDER = ? AND IDRECV = ?)";
+		//idmessage, idsender, idreceiver, contenu, Datetimemessage
+
+		try ( PreparedStatement pstmt  = conn.prepareStatement(sql)){
+
+			// set the value
+			pstmt.setString(1, myId);;
+			pstmt.setString(2, theirID);
+			pstmt.setString(3, theirID);;
+			pstmt.setString(4, myId);
+
+			ResultSet rs  = pstmt.executeQuery();
+			// loop through the result set
+			while (rs.next()) {
+				String contenu=rs.getString("CONTENU");
+				String sender= rs.getString("IDSENDER");
+				String recv = rs.getString("IDRECV");
+				Timestamp tsI= rs.getTimestamp("DATEMESSAGE");
+				Message messI = new Message(contenu,sender,recv,tsI);
+				result.add(messI);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return result;
+	}
+	public class Message{
+		String contenu;
+		String idSender;
+		String idRecv;
+		Timestamp date;
+
+		public Message(String contenu, String idSender, String idRecv, Timestamp date) {
+			this.contenu = contenu;
+			this.idSender = idSender;
+			this.idRecv = idRecv;
+			this.date = date;
+		}
+	}
+   
    public static void main(String[] args) {
 	   DatabaseManager Db = new DatabaseManager();
 	   //
@@ -222,14 +268,14 @@ public class DatabaseManager {
 	   Db.createtablemessage();
 	   //System.out.println("Table message created successfully");
 	   Timestamp D = new Timestamp(System.currentTimeMillis());
-	   Db.insertmessage(3, 1, 2, "Coucou premier message", D);
-	   Db.insertuser(5, "xxRaveauxx");
-	   Db.insertuser(6, "xxOsornioxx");
+	   Db.insertmessage(3, "1", "2", "Coucou premier message", D);
+	   Db.insertuser("5", "xxRaveauxx");
+	   Db.insertuser("6", "xxOsornioxx");
 
 	   //System.out.println("Datetime OK and message added to DB");
-	   Db.selectHistorywithX(1, 2);
+	   Db.selectHistorywithX("1", "2");
 	   Db.getAnnuaire();
-	   Db.changerPseudo(5, "xxMatthosxx");
+	   Db.changerPseudo("5", "xxMatthosxx");
 	   Db.getAnnuaire();
 	   Db.getIdByLogin("xxMatthosxx");
 	   Db.getAnnuaire();
