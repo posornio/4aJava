b47a707e71dc1a1cd5ac076586aecd71db904660
posornio <94918853+posornio@users.noreply.github.com>
2022-12-08 09:35:45 +0100

    progres annuaire

--- ChatBox/src/MainForm.java
+++ ChatBox/src/MainForm.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
-import java.awt.*;
-import java.sql.Connection;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainForm extends JFrame {
     private JScrollPane ContactScroll;
-    private JScrollPane MessageScroll;
     private JButton AffAnnButt;
     private JButton ChangerPseudoButt;
     private JButton DeconnexionButt;
@@ -13,27 +15,72 @@ public class MainForm extends JFrame {
     private JPanel PaneCenter;
     private JPanel mainPanel;
     private JLabel nameLabel;
+    private JList<Object> list1;
+    private JScrollPane paneContact;
+    private JScrollPane paneMessages;
+    private JList list2;
+    private JTable table1;
 
 
     public MainForm(){
-        DatabaseManager Db = new DatabaseManager();
-        //Connection connection = Db.conn;
-        Db.dbinit();
+
         setContentPane(mainPanel);
         setTitle("ChatApp");
+        DatabaseManager Db = new DatabaseManager();
+
+        //Connection connection = Db.conn;
+        Db.dbinit();
         ArrayList<String> asAnnu = Db.getAnnuaireList();
-        JList<String> annuaireJL = new JList<>(asAnnu.toArray(new String[0]));
-        ContactScroll = new JScrollPane(annuaireJL);
-        setSize(450,300);
+        setSize(800,600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        list1 = new JList<Object>(asAnnu.toArray());
+        paneContact.add(list1);
+        list1.setEnabled(true);
+        paneContact.setEnabled(true);
+        System.out.println(asAnnu.toString());
+        System.out.println("asAnnu2");
+
         setVisible(true);
     }
 
     public static void main(String[] args) {
         MainForm myForm = new MainForm();
-    }
 
+    }
+    MouseListener mouseListener = new MouseAdapter() {
+        public void mouseClicked(MouseEvent e) {
+            if (e.getClickCount() == 2) {
+                DatabaseManager Db = new DatabaseManager();
+
+                Db.dbinit();
+                String selectedItem = (String) list1.getSelectedValue();
+                int myId =Db.getIdbyLoginInt("xxOsornioxx");
+
+                int theirId =Db.getIdbyLoginInt(selectedItem);
+                ArrayList<String> arrMsgStr = new ArrayList<String>();
+                ArrayList<DatabaseManager.Message> arrayMessages = Db.ArrayHistorywithX(myId,theirId);
+                //arrayMessages.
+
+               // list2 = new JList<Object>(asAnnu.toArray());
+                paneContact.add(list1);
+                list1.setEnabled(true);
+                // add selectedItem to your second list.
+
+
+            }
+        }
+    };
     private void createUIComponents() {
+        DatabaseManager Db = new DatabaseManager();
+
+        Db.dbinit();
+        ArrayList<String> asAnnu = Db.getAnnuaireList();
+        setSize(800,600);
+        list1 = new JList<Object>(asAnnu.toArray());
+        System.out.println("asAnnu!");
+        //paneContact.add(list1);
+
         // TODO: place custom component creation code here
     }
 }
